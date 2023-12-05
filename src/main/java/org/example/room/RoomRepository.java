f97ada5730d7f0a0ae74548239721ca41cd711fb
coolStory24 <mihail.furm@gmail.com>
2023-12-06 00:53:14 +0300

    database files linted

--- src/main/java/org/example/room/RoomRepository.java
+++ src/main/java/org/example/room/RoomRepository.java
@@ -2,7 +2,10 @@ package org.example.room;
 
 public interface RoomRepository {
   public Room getById(long id);
+
   public long create(Room room);
+
   public void delete(long id);
+
   public void update(long id);
 }
--- src/main/java/org/example/user/UserRepository.java
+++ src/main/java/org/example/user/UserRepository.java
@@ -3,8 +3,11 @@ package org.example.user;
 public interface UserRepository {
   public interface RoomRepository {
     public User getById(long id);
+
     public long create(User user);
+
     public void delete(long id);
+
     public void update(long id);
   }
 }
