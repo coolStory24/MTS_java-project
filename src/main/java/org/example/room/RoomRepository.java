200e4075b9d3055e3aa1bdc264c4475260f9276b
coolStory24 <mihail.furm@gmail.com>
2023-12-06 12:55:37 +0300

    throw on a new line in RoomRepository.update method

--- src/main/java/org/example/room/RoomRepository.java
+++ src/main/java/org/example/room/RoomRepository.java
@@ -5,10 +5,10 @@ public interface RoomRepository {
 
   RoomEntity getById(long id) throws RoomExceptions.RoomDatabaseException;
 
-  long create(String title, String start, String end)
-    throws RoomExceptions.RoomDatabaseException;
+  long create(String title, String start, String end) throws RoomExceptions.RoomDatabaseException;
 
   void delete(long id);
 
-  void update(long id, String title, String start, String end) throws RoomExceptions.RoomDatabaseException;
+  void update(long id, String title, String start, String end)
+      throws RoomExceptions.RoomDatabaseException;
 }
