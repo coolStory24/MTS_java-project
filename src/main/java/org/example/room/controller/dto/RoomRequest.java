d84a2068dffa289be8d38f40aef0c7bd59b62093
coolStory24 <mihail.furm@gmail.com>
2023-12-12 15:59:05 +0300

    RoomRequest lint

--- src/main/java/org/example/room/controller/dto/RoomRequest.java
+++ src/main/java/org/example/room/controller/dto/RoomRequest.java
@@ -2,6 +2,5 @@ package org.example.room.controller.dto;
 
 public class RoomRequest {
   public record CreateRoom(String title, String start, String end) {}
-
   public record UpdateRoom(Long id, String title, String start, String end) {}
 }
