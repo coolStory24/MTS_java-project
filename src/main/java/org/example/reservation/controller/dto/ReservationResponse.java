a851c811b52dc7589e9d5c7e3cea6288eeb7ad90
anastaishafilonova <filonova.ai@phystech.edu>
2023-12-19 13:17:49 +0300

    edit format

--- src/main/java/org/example/reservation/controller/dto/ReservationResponse.java
+++ src/main/java/org/example/reservation/controller/dto/ReservationResponse.java
@@ -6,11 +6,9 @@ public class ReservationResponse {
   public record FindReservation(
       Long id, String start, String end, String startDay, Long userId, Long roomId) {}
 
-  public record FindReservationsForUser(
-      List<FindReservation> reservationsForUser) {}
+  public record FindReservationsForUser(List<FindReservation> reservationsForUser) {}
 
-  public record FindReservationsForRoom(
-      List<FindReservation> reservationsForRoom) {}
+  public record FindReservationsForRoom(List<FindReservation> reservationsForRoom) {}
 
   public record CreateReservation(long reservationId) {}
 }
