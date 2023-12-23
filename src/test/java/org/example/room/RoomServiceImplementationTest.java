042269635766da7ed2b802b1dcaf488d1e88edd1
Svyatoslav Sharov <sharov.so@phystech.edu>
2023-12-23 22:19:09 +0300

    Новый тест для класса RoomServiceImplementation

--- src/test/java/org/example/room/RoomServiceImplementationTest.java
+++ src/test/java/org/example/room/RoomServiceImplementationTest.java
@@ -6,20 +6,23 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Testcontainers
 class RoomServiceImplementationTest {
 
   @Container
   public static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:13");
-
+  private static final Logger LOG = Logger.getLogger(RoomServiceImplementationTest.class.getName());
   private static Jdbi jdbi;
 
   @BeforeAll
   static void beforeAll(){
     String postgresJdbcUrl = POSTGRES.getJdbcUrl();
-    System.out.print(postgresJdbcUrl + " " + POSTGRES.getUsername());
+    LOG.info("PostgreSQL has been successfully initialized: " + postgresJdbcUrl + ", " +POSTGRES.getUsername());
     jdbi = Jdbi.create(postgresJdbcUrl, POSTGRES.getUsername(), POSTGRES.getPassword());
   }
 
