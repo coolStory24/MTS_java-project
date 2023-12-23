f143759d711e5716f7d84b3a49202a546a02168d
Svyatoslav Sharov <sharov.so@phystech.edu>
2023-12-23 22:37:35 +0300

    Новый тест для метода createRoom класса RoomServiceImplementation

--- src/test/java/org/example/room/RoomServiceImplementationTest.java
+++ src/test/java/org/example/room/RoomServiceImplementationTest.java
@@ -1,5 +1,6 @@
 package org.example.room;
 
+import org.flywaydb.core.Flyway;
 import org.jdbi.v3.core.Jdbi;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.LocalTime;
 import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,6 +25,13 @@ class RoomServiceImplementationTest {
   @BeforeAll
   static void beforeAll(){
     String postgresJdbcUrl = POSTGRES.getJdbcUrl();
+    Flyway flyway =
+            Flyway.configure()
+                    .outOfOrder(true)
+                    .locations("classpath:db/migrations")
+                    .dataSource(postgresJdbcUrl, POSTGRES.getUsername(), POSTGRES.getPassword())
+                    .load();
+    flyway.migrate();
     LOG.info("PostgreSQL has been successfully initialized: " + postgresJdbcUrl + ", " +POSTGRES.getUsername());
     jdbi = Jdbi.create(postgresJdbcUrl, POSTGRES.getUsername(), POSTGRES.getPassword());
   }
@@ -29,7 +39,7 @@ class RoomServiceImplementationTest {
   @BeforeEach
 
   void beforeEach() {
-
+    jdbi.useTransaction(handle -> handle.createUpdate("DELETE FROM room").execute());
   }
 
   @Test
@@ -38,6 +48,18 @@ class RoomServiceImplementationTest {
 
   @Test
   void createRoom() {
+    RoomRepository roomRepository = new RoomRepositoryImplementation(jdbi);
+    RoomServiceImplementation roomService = new RoomServiceImplementation(roomRepository);
+
+    long roomId = roomService.createRoom(
+            "Room #1",
+            LocalTime.parse("10:00:00"),
+            LocalTime.parse("22:00:00"));
+    RoomRepository.RoomEntity room = roomService.getRoomById(roomId);
+    assertEquals("Room #1", room.title());
+    assertEquals(LocalTime.parse("10:00:00"), room.startInterval());
+    assertEquals(LocalTime.parse("22:00:00"), room.endInterval());
+    assertEquals(roomId, room.id());
   }
 
   @Test
