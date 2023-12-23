451bc96c846e8c7bc02c92d00f8e9b07518232cd
Svyatoslav Sharov <sharov.so@phystech.edu>
2023-12-23 23:37:23 +0300

    Написаны все тесты для методов класса UserServiceImplementation

--- src/test/java/org/example/user/UserServiceImplementationTest.java
+++ src/test/java/org/example/user/UserServiceImplementationTest.java
@@ -4,8 +4,10 @@ import org.flywaydb.core.Flyway;
 import org.jdbi.v3.core.Jdbi;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.logging.Logger;
@@ -15,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @Testcontainers
 class UserServiceImplementationTest {
 
+  @Container
   public static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:13");
   private static final Logger LOG = Logger.getLogger(UserServiceImplementationTest.class.getName());
   private static Jdbi jdbi;
@@ -36,22 +39,67 @@ class UserServiceImplementationTest {
   @BeforeEach
 
   void beforeEach() {
-    jdbi.useTransaction(handle -> handle.createUpdate("DELETE FROM room").execute());
+    jdbi.useTransaction(handle -> handle.createUpdate("DELETE FROM user").execute());
   }
 
   @Test
+  @DisplayName("UserServiceImplementationTest -- tests findUserById functionality")
   void findUserById() {
+    UserRepository userRepository = new UserRepositoryImplementation(jdbi);
+    UserServiceImplementation userService = new UserServiceImplementation(userRepository);
+
+    long userId = userService.createUser("User to find");
+
+    UserRepository.UserEntity foundUser = userService.findUserById(userId);
+
+    assertEquals("User to find", foundUser.name());
+    assertEquals(userId, foundUser.id());
+
   }
 
   @Test
-  void createUser() {
+  @DisplayName("UserServiceImplementationTest -- tests createUser functionality")
+  void shouldCreateUser() {
+    UserRepository userRepository = new UserRepositoryImplementation(jdbi);
+    UserServiceImplementation userService = new UserServiceImplementation(userRepository);
+
+    long userId = userService.createUser("John Smith");
+    UserRepository.UserEntity user = userService.findUserById(userId);
+
+    assertEquals("John Smith", user.name());
+    assertEquals(userId, user.id());
+
   }
 
   @Test
-  void updateUser() {
+  @DisplayName("UserServiceImplementationTest -- tests updateUser functionality")
+  void shouldUpdateUser() {
+    UserRepository userRepository = new UserRepositoryImplementation(jdbi);
+    UserServiceImplementation userService = new UserServiceImplementation(userRepository);
+
+    long userId = userService.createUser("User to update");
+    assertNotNull(userService.findUserById(userId));
+
+    userService.updateUser(userId,"Updated user");
+    UserRepository.UserEntity updatedUser = userService.findUserById(userId);
+
+    assertEquals("Updated user", updatedUser.name());
+    assertEquals(userId, updatedUser.id());
+
   }
 
   @Test
-  void deleteUser() {
+  @DisplayName("UserServiceImplementationTest -- tests deleteUser functionality")
+  void shouldDeleteUser() {
+    UserRepository userRepository = new UserRepositoryImplementation(jdbi);
+    UserServiceImplementation userService = new UserServiceImplementation(userRepository);
+
+    long userId = userService.createUser("User to delete");
+
+    assertNotNull(userService.findUserById(userId));
+
+    userService.deleteUser(userId);
+
+    assertThrows(UserExceptions.UserNotFoundException.class, () -> userService.findUserById(userId));
   }
 }
