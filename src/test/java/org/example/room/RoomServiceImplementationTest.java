c446005a09f36244c8225962915937c884a0be3c
Svyatoslav Sharov <sharov.so@phystech.edu>
2023-12-24 14:09:39 +0300

    Поправил форматирование

--- src/test/java/org/example/room/RoomServiceImplementationTest.java
+++ src/test/java/org/example/room/RoomServiceImplementationTest.java
@@ -20,6 +20,7 @@ class RoomServiceImplementationTest {
 
   @Container
   public static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:13");
+
   private static final Logger LOG = Logger.getLogger(RoomServiceImplementationTest.class.getName());
   private static Jdbi jdbi;
 
@@ -27,13 +28,17 @@ class RoomServiceImplementationTest {
   static void beforeAll() {
     String postgresJdbcUrl = POSTGRES.getJdbcUrl();
     Flyway flyway =
-            Flyway.configure()
-                    .outOfOrder(true)
-                    .locations("classpath:db/migrations")
-                    .dataSource(postgresJdbcUrl, POSTGRES.getUsername(), POSTGRES.getPassword())
-                    .load();
+        Flyway.configure()
+            .outOfOrder(true)
+            .locations("classpath:db/migrations")
+            .dataSource(postgresJdbcUrl, POSTGRES.getUsername(), POSTGRES.getPassword())
+            .load();
     flyway.migrate();
-    LOG.info("PostgreSQL has been successfully initialized: " + postgresJdbcUrl + ", " + POSTGRES.getUsername());
+    LOG.info(
+        "PostgreSQL has been successfully initialized: "
+            + postgresJdbcUrl
+            + ", "
+            + POSTGRES.getUsername());
     jdbi = Jdbi.create(postgresJdbcUrl, POSTGRES.getUsername(), POSTGRES.getPassword());
   }
 
@@ -48,10 +53,8 @@ class RoomServiceImplementationTest {
     RoomRepository roomRepository = new RoomRepositoryImplementation(jdbi);
     RoomServiceImplementation roomService = new RoomServiceImplementation(roomRepository);
 
-    long roomId = roomService.createRoom(
-            "Room #1",
-            LocalTime.parse("10:00:00"),
-            LocalTime.parse("22:00:00"));
+    long roomId =
+        roomService.createRoom("Room #1", LocalTime.parse("10:00:00"), LocalTime.parse("22:00:00"));
 
     RoomRepository.RoomEntity room = roomService.getRoomById(roomId);
 
@@ -67,10 +70,8 @@ class RoomServiceImplementationTest {
     RoomRepository roomRepository = new RoomRepositoryImplementation(jdbi);
     RoomServiceImplementation roomService = new RoomServiceImplementation(roomRepository);
 
-    long roomId = roomService.createRoom(
-            "Room #1",
-            LocalTime.parse("10:00:00"),
-            LocalTime.parse("22:00:00"));
+    long roomId =
+        roomService.createRoom("Room #1", LocalTime.parse("10:00:00"), LocalTime.parse("22:00:00"));
     RoomRepository.RoomEntity room = roomService.getRoomById(roomId);
     assertEquals("Room #1", room.title());
     assertEquals(LocalTime.parse("10:00:00"), room.startInterval());
@@ -84,16 +85,10 @@ class RoomServiceImplementationTest {
     RoomRepository roomRepository = new RoomRepositoryImplementation(jdbi);
     RoomServiceImplementation roomService = new RoomServiceImplementation(roomRepository);
 
-    long roomId = roomService.createRoom(
-            "Room #1",
-            LocalTime.parse("10:00:00"),
-            LocalTime.parse("22:00:00"));
+    long roomId =
+        roomService.createRoom("Room #1", LocalTime.parse("10:00:00"), LocalTime.parse("22:00:00"));
     roomService.updateRoom(
-            roomId,
-            "Room #1 (updated)",
-            LocalTime.parse("07:00:00"),
-            LocalTime.parse("19:00:00")
-    );
+        roomId, "Room #1 (updated)", LocalTime.parse("07:00:00"), LocalTime.parse("19:00:00"));
     RoomRepository.RoomEntity room = roomService.getRoomById(roomId);
 
     assertEquals("Room #1 (updated)", room.title());
